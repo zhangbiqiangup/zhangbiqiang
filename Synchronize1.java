ceef4acba0fb5325f91a9fdce1f0a10960c33024
zhangbiqiangup <46088117+zhangbiqiangup@users.noreply.github.com>
2018-12-24 01:19:38 +0800

    Update and rename synchronize1 to Synchronize1.java

--- Synchronize1.java
+++ Synchronize1.java
@@ -1,7 +1,7 @@
 /**
 ** synchronize修饰一个代码块
 ** 一个线程访问一个对象中的synchronized(this)同步代码块时，其他试图访问该对象的线程将被阻塞
-**/
+*/
 class SyncThread implements Runnable {
 	   private static int count;
  
@@ -42,4 +42,21 @@ public class Demo00 {
 		t2.start();
 	}
 }
+
+/**
+输出结果：
+Thread-0:0
+Thread-0:1
+Thread-0:2
+Thread-0:3
+Thread-0:4
+Thread-1:5
+Thread-1:6
+Thread-1:7
+Thread-1:8
+Thread-1:9
+
+当两个并发线程(thread1和thread2)访问同一个对象(syncThread)中的synchronized代码块时，在同一时刻只能有一个线程得到执行，另一个线程受阻塞，
+必须等待当前线程执行完这个代码块以后才能执行该代码块
+*/
 
